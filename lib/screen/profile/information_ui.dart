d75dabc34ac0785dd489b20b28bad57a069570b4
Novian Andika <hello@fleetime.my.id>
2023-03-22 07:25:34 +0700

    fix: revisi iformation

--- lib/screen/profile/information_ui.dart
+++ lib/screen/profile/information_ui.dart
@@ -7,78 +7,90 @@ class InformationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBarReading(
-          title: 'Info',
-          context: context,
-        ),
-        body: Column(
+      appBar: AppBarReading(
+        title: 'Info',
+        context: context,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: [
             Container(
               padding: const EdgeInsets.only(
                   top: 40, bottom: 30, left: 30, right: 30),
-              width: double.infinity,
               child: const Image(
                 image: AssetImage('assets/images/information.png'),
               ),
             ),
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 50),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Catat, Kalender Pendidikan Tahun Ajaran 2021/2022 Terbaru!',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                    textAlign: TextAlign.justify,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  RichText(
-                    textAlign: TextAlign.justify,
-                    text: const TextSpan(
-                      style: TextStyle(fontSize: 16, color: Colors.black),
-                      text:
-                          'Mau tahu jatwal libur dan ujian di tahun ajaran kali ini? Agar tidak ada jatwal terlewat, penting banget nih untuk kamu mengetahui ',
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'Kalender Pendidikan ',
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.black,
-                            decorationThickness: 2,
-                          ),
-                        ),
-                        TextSpan(
-                          text:
-                              'tahun ajaran 2021/2022. Walaupun kita masih berada di tengah-tengah pandemi, kegiatan belajar kamu tidak boleh sampai terganggu.',
-                        ),
-                      ],
+                children: const [
+                  ItalicizedWordText(
+                    wordToItalicize: 'Kalender Pendidikan',
+                    italicStyle: TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Nah, bagi kamu yang ingin tahu mengenai info kegiatan dan momen-momen penting di sekolah selama satu tahun kedepan, kalender pendidikan ini harus kamu simak sebaik-baiknya. Di sini, kamu bisa melihat periode ujian, hari libur nasional, jatwal tahun ajaran baru, hingga jatwal pelaksanaan SNMPTN dan UTBK SBMPTN 2022',
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                    textAlign: TextAlign.justify,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Yuk, cek jatwal kalender pendidikan tahun ajaran 2021/2022 berikut ini. Jatwal dibawah ini mengacu pada Dinas Pendidikan DKI Jakarta dan dapat berbeda di tiap daerah sesuai dengan kebijakan pemerintah daerah masing-masing.',
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                    textAlign: TextAlign.justify,
+                    fullText:
+                        'Mau tahu jadwal libur dan ujian di tahun ajaran kali ini? Agar tidak ada jadwal terlewat, penting banget nih untuk kamu mengetahui Kalender Pendidikan tahun ajaran 2021/2022. Walaupun kita masih berada di tengah-tengah pandemi, kegiatan belajar kamu tidak boleh sampai terganggu.\n\n'
+                        'Nah, bagi kamu yang ingin tahu mengenai info kegiatan dan momen-momen penting di sekolah selama satu tahun ke depan, kalender pendidikan ini harus kamu simak sebaik-baiknya. Di sini, kamu bisa melihat periode ujian, hari libur nasional, jadwal tahun ajaran baru, hingga jadwal pelaksanaan SNMPTN dan UTBK SBMPTN 2022.\n\n'
+                        'Yuk, cek jadwal kalender pendidikan tahun ajaran 2021/2022 berikut ini. Jadwal di bawah ini mengacu pada Dinas Pendidikan DKI Jakarta dan dapat berbeda di tiap daerah sesuai dengan kebijakan pemerintah daerah masing-masing.',
                   ),
                 ],
               ),
             )
           ],
-        ));
+        ),
+      ),
+    );
+  }
+}
+
+class ItalicizedWordText extends StatelessWidget {
+  final String wordToItalicize;
+  final String fullText;
+  final TextStyle? textStyle;
+  final TextStyle? italicStyle;
+
+  const ItalicizedWordText({
+    Key? key,
+    required this.wordToItalicize,
+    required this.fullText,
+    this.textStyle,
+    this.italicStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = this.textStyle ??
+        const TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+            height: 1.5);
+    final italicStyle = this.italicStyle ??
+        const TextStyle(
+          fontSize: 25,
+          fontStyle: FontStyle.italic,
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+        );
+
+    final indexOfWord = fullText.indexOf(wordToItalicize);
+    final wordEndIndex = indexOfWord + wordToItalicize.length;
+
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(text: fullText.substring(0, indexOfWord), style: textStyle),
+          TextSpan(text: wordToItalicize, style: italicStyle),
+          TextSpan(text: fullText.substring(wordEndIndex), style: textStyle),
+        ],
+      ),
+      textAlign: TextAlign.justify,
+    );
   }
 }
