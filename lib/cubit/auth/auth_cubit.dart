6b048770ed9387e63f9e3dc52f893395e8932530
Novian Andika <novian.andika@students.amikom.ac.id>
2023-04-04 08:41:35 +0700

    🔀 chore(auth_cubit.dart): reorder imports This commit reorders the imports in auth_cubit.dart to follow the convention of having core Dart imports first, followed by package imports, and then project imports. This improves the readability of the code and makes it easier to identify where different imports come from.

--- lib/cubit/auth/auth_cubit.dart
+++ lib/cubit/auth/auth_cubit.dart
@@ -1,10 +1,10 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter_huixin_app/data/models/auth/requests/login_request_model.dart';
-import 'package:flutter_huixin_app/data/models/auth/requests/register_request_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:flutter_huixin_app/data/datasources/auth_datasource.dart';
 import 'package:flutter_huixin_app/data/models/auth/auth_response_model.dart';
+import 'package:flutter_huixin_app/data/models/auth/requests/login_request_model.dart';
+import 'package:flutter_huixin_app/data/models/auth/requests/register_request_model.dart';
 
 import '../../data/models/auth/requests/update_fcm_request_model.dart';
 import '../../data/models/auth/requests/update_profile_request_model.dart';
