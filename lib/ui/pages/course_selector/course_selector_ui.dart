c68ec6875f05f87e06bc85787f1c0be3c93512af
Novian Andika <novian.andika@students.amikom.ac.id>
2023-04-06 15:42:36 +0700

    🎨 style(course_selector_ui_master_group_tile.dart): rename MasterGroupMateriTile to CourseSelectorUiMasterGroupTile ✨ feat(course_selector_ui_master_group_tile.dart): add support for navigating to course detail page 🎨 style(course_selector_ui_master_group_tile_loading.dart): rename MasterGroupMateriTileLoading to CourseSelectorUiMasterGroupTileLoading The class name MasterGroupMateriTile has been renamed to CourseSelectorUiMasterGroupTile to better reflect its purpose. Support for navigating to the course detail page has been added. The class name MasterGroupMateriTileLoading has been renamed to CourseSelectorUiMasterGroupTileLoading to better reflect its purpose.
    🎨 style(course_selector_ui.dart): remove unused imports and reformat code
    The commit removes unused imports and reformats the code to improve readability.
    
    🚀 feat(course_selector_ui.dart): add support for passing level name as an argument to the course selector page
    The commit adds support for passing the level name as an argument to the course selector page. This allows the course selector page to display the level name in the app bar.

--- lib/ui/pages/course_selector/course_selector_ui.dart
+++ lib/ui/pages/course_selector/course_selector_ui.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_huixin_app/cubit/mastering/master_group_materi/master_group_materi_cubit.dart';
 import 'package:flutter_huixin_app/data/datasources/local/app_secure_storage.dart';
 import 'package:flutter_huixin_app/data/models/auth/auth_response_model.dart';
+import 'package:flutter_huixin_app/ui/pages/course_selector/components/course_selector_ui_master_group_tile.dart';
+import 'package:flutter_huixin_app/ui/pages/course_selector/components/course_selector_ui_master_group_tile_loading.dart';
 
-import '../../../common/constants/color.dart';
-import '../../../cubit/entities/course_selector.dart';
 import '../../widgets/appbar/appbar_style.dart';
 import '../../widgets/bottom_appbar_note.dart';
 
@@ -48,17 +47,9 @@ class _CourseSelectorState extends State<CourseSelector> {
 
   @override
   Widget build(BuildContext context) {
-    /// Get arguments from previous page
-
     return Scaffold(
       appBar: AppBarCourse(
-        title: context.select(
-          (MasterGroupMateriCubit masterMateriCubit) =>
-              masterMateriCubit.state.maybeMap(
-            orElse: () => '..',
-            loaded: (state) => state.data.data?.first.name ?? '..',
-          ),
-        ),
+        title: args?['level_name'] ?? '..',
         progression: '5/12',
         context: context,
       ),
@@ -67,84 +58,42 @@ class _CourseSelectorState extends State<CourseSelector> {
         child: Column(
           children: [
             Expanded(
-              child: GridView.builder(
-                itemCount: allCourseSelector.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  childAspectRatio: 0.8,
-                ),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      allCourseSelector[index].routeName != ''
-                          ? Navigator.pushNamed(
-                              context, allCourseSelector[index].routeName)
-                          : null;
-                    },
-                    child: Card(
-                      color: AppColors.whiteColor,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Card(
-                              child: allCourseSelector[index].isUnlocked == true
-                                  ? Container(
-                                      color: AppColors.bottom,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Center(
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: [
-                                              Text(
-                                                allCourseSelector[index].day,
-                                                style: const TextStyle(
-                                                  color: AppColors.whiteColor,
-                                                  fontSize: 20,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                              const SizedBox(height: 8),
-                                              Image.asset(
-                                                allCourseSelector[index]
-                                                    .logoUrl,
-                                                height: 50,
-                                                fit: BoxFit.fill,
-                                              ),
-                                              const SizedBox(height: 8),
-                                              Image.asset(
-                                                "assets/images/progress_bar.png",
-                                                fit: BoxFit.fill,
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                  : Container(
-                                      color: AppColors.whiteColor2,
-                                      child: Center(
-                                        child: Image.asset(
-                                          "assets/images/lock.png",
-                                          height: 30,
-                                          fit: BoxFit.fill,
-                                        ),
-                                      ),
-                                    ),
-                            ),
-                          ),
-                        ]
-                            .animate(interval: 100.ms)
-                            .fadeIn(duration: 200.ms, delay: 400.ms)
-                            .shimmer(
-                                blendMode: BlendMode.srcOver,
-                                color: Colors.white12)
-                            .move(
-                                begin: const Offset(-16, 0),
-                                curve: Curves.easeOutQuad),
-                      ),
+              child:
+                  BlocBuilder<MasterGroupMateriCubit, MasterGroupMateriState>(
+                builder: (context, state) {
+                  return GridView.builder(
+                    itemCount: state.maybeMap(
+                      orElse: () => 6,
+                      loaded: (state) => state.data.data?.length ?? 0,
                     ),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      childAspectRatio: 0.8,
+                    ),
+                    itemBuilder: (context, index) {
+                      return state.when(
+                        initial: () {
+                          return null;
+                        },
+                        loading: () {
+                          return const MasterGroupMateriTileLoading();
+                        },
+                        loaded: (data) {
+                          return MasterGroupMateriTile(
+                            index: index,
+                            state: state,
+                            dayName: data.data![index].name ?? '',
+                            imageUrl: data.data![index].imgFile ?? '',
+                          );
+                        },
+                        error: (message) {
+                          return Center(
+                            child: Text(message),
+                          );
+                        },
+                      );
+                    },
                   );
                 },
               ),
