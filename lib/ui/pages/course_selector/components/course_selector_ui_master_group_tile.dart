cf499fbbf85dd0bb0beb47bc4ff39639baf06549
Novian Andika <hello@fleetime.my.id>
2023-04-06 19:01:50 +0700

    🔥 refactor(course_selector_ui_master_group_tile.dart): remove unused import statement
    🔨 refactor(course_selector_ui_master_group_tile.dart): refactor onTap function to use state.maybeMap() instead of if-else statements
    
    🔥 refactor(home_ui_tile_master.dart): remove unused import statement
    
    🎨 style(home_ui_tile_master.dart): reformat code for better readability

--- lib/ui/pages/course_selector/components/course_selector_ui_master_group_tile.dart
+++ lib/ui/pages/course_selector/components/course_selector_ui_master_group_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_huixin_app/common/constants/color.dart';
 import 'package:flutter_huixin_app/cubit/mastering/master_group_materi/master_group_materi_cubit.dart';
 
@@ -21,116 +20,120 @@ class MasterGroupMateriTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        // allCourseSelector[index].routeName != ''
-        //     ? Navigator.pushNamed(context, allCourseSelector[index].routeName)
-        //     : null;
+        state.maybeMap(
+          orElse: () => null,
+          loaded: (state) {
+            if (index == 0) {
+              Navigator.pushNamed(
+                context,
+                '/course_initial',
+                arguments: {
+                  'level_id': state.data.data![index].idLevel,
+                  'level_id_materi': state.data.data![index].idGroupMateri,
+                },
+              );
+            }
+          },
+        );
       },
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
         color: AppColors.whiteColor,
-        child: Column(
-          children: [
-            Expanded(
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: state.maybeMap(
-                  orElse: () => null,
-                  loaded: (state) {
-                    if (index == 0) {
-                      return Container(
-                        color: AppColors.bottom,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  dayName,
-                                  style: const TextStyle(
-                                    color: AppColors.whiteColor,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+        child: Column(children: [
+          Expanded(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: state.maybeMap(
+                orElse: () => null,
+                loaded: (state) {
+                  if (index == 0) {
+                    return Container(
+                      color: AppColors.bottom,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                dayName,
+                                style: const TextStyle(
+                                  color: AppColors.whiteColor,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                const SizedBox(height: 8),
-                                Image.network(
-                                  'https://huixin.id/assets/group_materi/$imageUrl',
-                                  height: 50,
-                                  fit: BoxFit.fill,
-                                ),
-                                const SizedBox(height: 8),
-                                Image.asset(
-                                  "assets/images/progress_bar.png",
-                                  fit: BoxFit.fill,
-                                ),
-                              ],
-                            ),
+                              ),
+                              const SizedBox(height: 8),
+                              Image.network(
+                                'https://huixin.id/assets/group_materi/$imageUrl',
+                                height: 50,
+                                fit: BoxFit.fill,
+                              ),
+                              const SizedBox(height: 8),
+                              Image.asset(
+                                "assets/images/progress_bar.png",
+                                fit: BoxFit.fill,
+                              ),
+                            ],
                           ),
                         ),
-                      );
-                    } else {
-                      return (state.data.data![index].reportReading!
-                                  .isNotEmpty ||
-                              state
-                                  .data.data![index].reportSpeaking!.isNotEmpty)
-                          ? Container(
-                              color: AppColors.bottom,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Center(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        dayName,
-                                        style: const TextStyle(
-                                          color: AppColors.whiteColor,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Image.network(
-                                        'https://huixin.id/assets/group_materi/$imageUrl',
-                                        height: 50,
-                                        fit: BoxFit.fill,
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Image.asset(
-                                        "assets/images/progress_bar.png",
-                                        fit: BoxFit.fill,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            )
-                          : Container(
-                              color: AppColors.whiteColor2,
+                      ),
+                    );
+                  } else {
+                    return (state.data.data![index].reportReading!.isNotEmpty ||
+                            state.data.data![index].reportSpeaking!.isNotEmpty)
+                        ? Container(
+                            color: AppColors.bottom,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
                               child: Center(
-                                child: Image.asset(
-                                  "assets/images/lock.png",
-                                  height: 30,
-                                  fit: BoxFit.fill,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      dayName,
+                                      style: const TextStyle(
+                                        color: AppColors.whiteColor,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Image.network(
+                                      'https://huixin.id/assets/group_materi/$imageUrl',
+                                      height: 50,
+                                      fit: BoxFit.fill,
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Image.asset(
+                                      "assets/images/progress_bar.png",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ],
                                 ),
                               ),
-                            );
-                    }
-                  },
-                ),
+                            ),
+                          )
+                        : Container(
+                            color: AppColors.whiteColor2,
+                            child: Center(
+                              child: Image.asset(
+                                "assets/images/lock.png",
+                                height: 30,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          );
+                  }
+                },
               ),
             ),
-          ]
-              .animate(interval: 100.ms)
-              .fadeIn(duration: 200.ms, delay: 400.ms)
-              .shimmer(blendMode: BlendMode.srcOver, color: Colors.white12)
-              .move(begin: const Offset(-16, 0), curve: Curves.easeOutQuad),
-        ),
+          ),
+        ]),
       ),
     );
   }
--- lib/ui/pages/home/components/home_ui_tile_master.dart
+++ lib/ui/pages/home/components/home_ui_tile_master.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_huixin_app/common/constants/color.dart';
 import 'package:flutter_huixin_app/cubit/mastering/master_level/master_level_cubit.dart';
 
@@ -37,131 +36,123 @@ class TileMasterLevel extends StatelessWidget {
       },
       child: Card(
         color: AppColors.whiteColor,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Image.asset(
-                    "assets/images/crown.png",
-                    height: 10,
-                    fit: BoxFit.fill,
-                  ),
-                  const SizedBox(
-                    width: 4,
-                  ),
-                  const Text(
-                    '1/2',
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset(
+                  "assets/images/crown.png",
+                  height: 10,
+                  fit: BoxFit.fill,
                 ),
-                child: state.maybeMap(
-                  orElse: () => null,
-                  loaded: (state) {
-                    if (index == 0) {
-                      return Container(
-                        color: AppColors.bottom,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  levelName,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                const SizedBox(
+                  width: 4,
+                ),
+                const Text(
+                  '1/2',
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: state.maybeMap(
+                orElse: () => null,
+                loaded: (state) {
+                  if (index == 0) {
+                    return Container(
+                      color: AppColors.bottom,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                levelName,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                const SizedBox(height: 8),
-                                Image.network(
-                                  'https://huixin.id/assets/level/$levelImage',
-                                  height: 80,
-                                  fit: BoxFit.fill,
-                                ),
-                                const SizedBox(height: 8),
-                                Image.asset(
-                                  "assets/images/progress_bar.png",
-                                  fit: BoxFit.fill,
-                                ),
-                              ],
-                            ),
+                              ),
+                              const SizedBox(height: 8),
+                              Image.network(
+                                'https://huixin.id/assets/level/$levelImage',
+                                height: 80,
+                                fit: BoxFit.fill,
+                              ),
+                              const SizedBox(height: 8),
+                              Image.asset(
+                                "assets/images/progress_bar.png",
+                                fit: BoxFit.fill,
+                              ),
+                            ],
                           ),
                         ),
-                      );
-                    } else {
-                      return (state.data.data![index].reportReading!
-                                  .isNotEmpty ||
-                              state
-                                  .data.data![index].reportSpeaking!.isNotEmpty)
-                          ? Container(
-                              color: AppColors.bottom,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Center(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        levelName,
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Image.network(
-                                        'https://huixin.id/assets/level/$levelImage',
-                                        height: 80,
-                                        fit: BoxFit.fill,
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Image.asset(
-                                        "assets/images/progress_bar.png",
-                                        fit: BoxFit.fill,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            )
-                          : Container(
-                              color: AppColors.whiteColor2,
+                      ),
+                    );
+                  } else {
+                    return (state.data.data![index].reportReading!.isNotEmpty ||
+                            state.data.data![index].reportSpeaking!.isNotEmpty)
+                        ? Container(
+                            color: AppColors.bottom,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
                               child: Center(
-                                child: Image.asset(
-                                  "assets/images/lock.png",
-                                  height: 30,
-                                  fit: BoxFit.fill,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      levelName,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Image.network(
+                                      'https://huixin.id/assets/level/$levelImage',
+                                      height: 80,
+                                      fit: BoxFit.fill,
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Image.asset(
+                                      "assets/images/progress_bar.png",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ],
                                 ),
                               ),
-                            );
-                    }
-                  },
-                ),
+                            ),
+                          )
+                        : Container(
+                            color: AppColors.whiteColor2,
+                            child: Center(
+                              child: Image.asset(
+                                "assets/images/lock.png",
+                                height: 30,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          );
+                  }
+                },
               ),
-            )
-          ]
-              .animate(interval: 100.ms)
-              .fadeIn(duration: 200.ms, delay: 400.ms)
-              .shimmer(blendMode: BlendMode.srcOver, color: Colors.white12)
-              .move(begin: const Offset(-16, 0), curve: Curves.easeOutQuad),
-        ),
+            ),
+          )
+        ]),
       ),
     );
   }
