307863ddadb4965987457569ad441dead6cf3ec9
Novian Andika <novian.andika@students.amikom.ac.id>
2023-04-06 12:19:02 +0700

    🔥 refactor(home_ui.dart): remove unused imports and variables This commit removes unused imports and variables in the home_ui.dart file. The import for the course_selector_ui.dart file is removed as it is not used in the file. The imports for the active_students.dart and home_course.dart files are removed as they are not used in the file. The studentsData variable is also removed as it is not used in the file.
    ✨ feat(home_ui.dart): add MasterLevelCubit to fetch master level data
    This commit adds the MasterLevelCubit to the home_ui.dart file to fetch the master level data. The getMasterLevel method is called in the initState method to fetch the data when the page is loaded. The data is then displayed in the UI.
    
    🔨 refactor(home_ui.dart): extract TileMasterLevel widget
    The TileMasterLevel widget is extracted from the HomeItems widget to improve readability and maintainability of the code. The TileMasterLevel widget is responsible for rendering the individual tiles in the GridView.
    
    🚀 feat(providers.dart): add MasterLevelCubit to AppProviders
    The MasterLevelCubit is added to the AppProviders, which provides the necessary dependencies for the MasterLevelCubit to function. This allows the MasterLevelCubit to be used throughout the application.

--- lib/ui/pages/home/home_ui.dart
+++ lib/ui/pages/home/home_ui.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_huixin_app/cubit/home/active_student/active_student_cubit.dart';
 import 'package:flutter_huixin_app/cubit/home/daily_activity/daily_activity_cubit.dart';
 import 'package:flutter_huixin_app/cubit/home/xp/xp_cubit.dart';
+import 'package:flutter_huixin_app/cubit/mastering/master_level/master_level_cubit.dart';
 import 'package:flutter_huixin_app/data/datasources/local/app_secure_storage.dart';
 import 'package:flutter_huixin_app/data/models/auth/auth_response_model.dart';
 import 'package:flutter_huixin_app/data/models/get_active_student_response_model.dart';
@@ -11,12 +12,8 @@ import 'package:random_avatar/random_avatar.dart';
 import 'package:shimmer/shimmer.dart';
 
 import '../../../common/constants/color.dart';
-import '../../../cubit/entities/active_students.dart';
-import '../../../cubit/entities/home_course.dart';
 import '../../widgets/appbar/appbar_style.dart';
 import '../../widgets/navigator_style.dart';
-import '../../widgets/pageroute.dart';
-import '../course_selector/course_selector_ui.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -42,6 +39,7 @@ class _HomePageState extends State<HomePage> {
       context.read<XpCubit>().getXp(user?.userId ?? '');
       context.read<DailyActivityCubit>().getDailyActivity(user?.userId ?? '');
       context.read<ActiveStudentCubit>().getActiveStudent();
+      context.read<MasterLevelCubit>().getMasterLevel(user?.userId ?? '');
     });
   }
 
@@ -57,11 +55,13 @@ class _HomePageState extends State<HomePage> {
             loaded: (state) => state.data.data?.first.jmlXp.toString(),
           ),
         ),
-        dailyActivity: context.select((DailyActivityCubit dailyActivityCubit) =>
-            dailyActivityCubit.state.maybeMap(
-              orElse: () => '..',
-              loaded: (state) => state.data.data?.first.jmlDaily.toString(),
-            )),
+        dailyActivity: context.select(
+          (DailyActivityCubit dailyActivityCubit) =>
+              dailyActivityCubit.state.maybeMap(
+            orElse: () => '..',
+            loaded: (state) => state.data.data?.first.jmlDaily.toString(),
+          ),
+        ),
       ),
       body: const HomeItems(),
       bottomNavigationBar: const Padding(
@@ -107,7 +107,10 @@ class HomeItems extends StatelessWidget {
                       child: ListView.separated(
                         separatorBuilder: (context, index) =>
                             const SizedBox(width: 8),
-                        itemCount: studentsData.length,
+                        itemCount: state.maybeMap(
+                          orElse: () => 6,
+                          loaded: (state) => state.data.data?.length ?? 0,
+                        ),
                         scrollDirection: Axis.horizontal,
                         itemBuilder: (context, index) {
                           return state.when(
@@ -145,114 +148,207 @@ class HomeItems extends StatelessWidget {
           ),
           const SizedBox(height: 16),
           Expanded(
-            child: GridView.builder(
-              itemCount: allHomeCourse.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: 0.7,
-              ),
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    allHomeCourse[index].isUnlocked == true
-                        ? Navigator.push(context,
-                            FadePageRoute(builder: (context) {
-                            return const CourseSelector();
-                          }))
-                        : null;
-                  },
-                  child: Card(
-                    color: AppColors.whiteColor,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Image.asset(
-                                "assets/images/crown.png",
-                                height: 10,
-                                fit: BoxFit.fill,
-                              ),
-                              const SizedBox(
-                                width: 4,
-                              ),
-                              Text(
-                                '${allHomeCourse[index].currentCourse}/${allHomeCourse[index].totalCourse}',
-                                style: const TextStyle(
-                                  color: Colors.black45,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Card(
-                            child: allHomeCourse[index].isUnlocked == true
-                                ? Container(
-                                    color: AppColors.bottom,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Center(
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                              allHomeCourse[index].courseName,
-                                              style: const TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            const SizedBox(height: 8),
-                                            Image.asset(
-                                              allHomeCourse[index].logoUrl,
-                                              height: 50,
-                                              fit: BoxFit.fill,
-                                            ),
-                                            const SizedBox(height: 8),
-                                            Image.asset(
-                                              "assets/images/progress_bar.png",
-                                              fit: BoxFit.fill,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  )
-                                : Container(
-                                    color: AppColors.whiteColor2,
-                                    child: Center(
-                                      child: Image.asset(
-                                        "assets/images/lock.png",
-                                        height: 30,
-                                        fit: BoxFit.fill,
-                                      ),
-                                    ),
-                                  ),
-                          ),
-                        ),
-                      ]
-                          .animate(interval: 100.ms)
-                          .fadeIn(duration: 200.ms, delay: 400.ms)
-                          .shimmer(
-                              blendMode: BlendMode.srcOver,
-                              color: Colors.white12)
-                          .move(
-                              begin: const Offset(-16, 0),
-                              curve: Curves.easeOutQuad),
-                    ),
+            child: BlocBuilder<MasterLevelCubit, MasterLevelState>(
+              builder: (context, state) {
+                return GridView.builder(
+                  itemCount: state.maybeMap(
+                    orElse: () => 12,
+                    loaded: (state) => state.data.data?.length ?? 0,
                   ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 0.7,
+                  ),
+                  itemBuilder: (context, index) {
+                    return state.when(
+                      initial: () {
+                        return null;
+                      },
+                      loading: () {
+                        return const CircularProgressIndicator();
+                      },
+                      loaded: (data) {
+                        return TileMasterLevel(
+                          index: index,
+                          levelName: data.data![index].name ?? '',
+                          levelImageUrl: data.data![index].imgUrl ?? '',
+                          levelImage: data.data![index].imgFile ?? '',
+                          state: state,
+                        );
+                      },
+                      error: (message) {
+                        return Text(message);
+                      },
+                    );
+                  },
                 );
               },
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class TileMasterLevel extends StatelessWidget {
+  final int index;
+  final String levelName;
+  final String levelImageUrl;
+  final String levelImage;
+  final MasterLevelState state;
+
+  const TileMasterLevel({
+    super.key,
+    required this.index,
+    required this.levelName,
+    required this.levelImage,
+    required this.levelImageUrl,
+    required this.state,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // allHomeCourse[index].isUnlocked == true
+        //     ? Navigator.push(
+        //         context,
+        //         FadePageRoute(
+        //           builder: (context) {
+        //             return const CourseSelector();
+        //           },
+        //         ),
+        //       )
+        //     : null;
+      },
+      child: Card(
+        color: AppColors.whiteColor,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Image.asset(
+                    "assets/images/crown.png",
+                    height: 10,
+                    fit: BoxFit.fill,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  const Text(
+                    '1/2',
+                    style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: state.maybeMap(
+                  orElse: () => null,
+                  loaded: (state) {
+                    if (index == 0) {
+                      return Container(
+                        color: AppColors.bottom,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  levelName,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Image.network(
+                                  'https://huixin.id/assets/level/$levelImage',
+                                  height: 80,
+                                  fit: BoxFit.fill,
+                                ),
+                                const SizedBox(height: 8),
+                                Image.asset(
+                                  "assets/images/progress_bar.png",
+                                  fit: BoxFit.fill,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    } else {
+                      return (state.data.data![index].reportReading!
+                                  .isNotEmpty ||
+                              state
+                                  .data.data![index].reportSpeaking!.isNotEmpty)
+                          ? Container(
+                              color: AppColors.bottom,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        levelName,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 8),
+                                      Image.network(
+                                        'https://huixin.id/assets/level/$levelImage',
+                                        height: 80,
+                                        fit: BoxFit.fill,
+                                      ),
+                                      const SizedBox(height: 8),
+                                      Image.asset(
+                                        "assets/images/progress_bar.png",
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Container(
+                              color: AppColors.whiteColor2,
+                              child: Center(
+                                child: Image.asset(
+                                  "assets/images/lock.png",
+                                  height: 30,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            );
+                    }
+                  },
+                ),
+              ),
+            )
+          ]
+              .animate(interval: 100.ms)
+              .fadeIn(duration: 200.ms, delay: 400.ms)
+              .shimmer(blendMode: BlendMode.srcOver, color: Colors.white12)
+              .move(begin: const Offset(-16, 0), curve: Curves.easeOutQuad),
+        ),
       ),
     );
   }
