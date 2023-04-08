286fc296b070d1e0209c8bcaaa8295639bbf0df8
Novian Andika <hello@fleetime.my.id>
2023-04-08 13:15:52 +0700

    🎨 style(home_ui_avatar_loaded.dart): replace RandomAvatar with CachedNetworkImage 🎨 style(home_ui_avatar_loaded.dart): add shimmer effect to CachedNetworkImage 🎨 style(home_ui_avatar_loaded.dart): add error widget to CachedNetworkImage 🎨 style(home_ui_tile_master_loading.dart): refactor widget tree 🎨 style(home_ui_tile_master_loading.dart): add shimmer effect to Card 🎨 style(home_ui_tile_master_loading.dart): add progress bar image 🎨 style(thunderclient.json): add isPath flag to token_api parameter

--- lib/ui/pages/home/components/home_ui_avatar_loaded.dart
+++ lib/ui/pages/home/components/home_ui_avatar_loaded.dart
@@ -1,6 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_huixin_app/data/models/get_active_student_response_model.dart';
-import 'package:random_avatar/random_avatar.dart';
+import 'package:shimmer/shimmer.dart';
 
 class AvatarLoader extends StatelessWidget {
   final int index;
@@ -16,11 +17,37 @@ class AvatarLoader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CircleAvatar(
-          radius: 30,
-          child: RandomAvatar(
-            data.data![index].fullName ?? '..',
-          ),
+        CachedNetworkImage(
+          imageUrl: data.data![index].imgFile == null
+              ? 'https://pwco.com.sg/wp-content/uploads/2020/05/Generic-Profile-Placeholder-v3-1536x1536.png'
+              : 'https://huixin.id/assets/fileuser/${data.data![index].imgFile}',
+          imageBuilder: (context, imageProvider) {
+            return CircleAvatar(
+              radius: 30,
+              backgroundImage: imageProvider,
+            );
+          },
+          placeholder: (context, url) {
+            return Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: CircleAvatar(
+                radius: 30,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            );
+          },
+          errorWidget: (context, url, error) {
+            return const CircleAvatar(
+              radius: 30,
+              child: Icon(Icons.error),
+            );
+          },
         ),
         const SizedBox(height: 8),
         Text(
--- lib/ui/pages/home/components/home_ui_tile_master_loading.dart
+++ lib/ui/pages/home/components/home_ui_tile_master_loading.dart
@@ -11,75 +11,77 @@ class TileMasterLevelLoading extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color: AppColors.whiteColor,
-      child: Column(children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Image.asset(
-                "assets/images/crown.png",
-                height: 10,
-                fit: BoxFit.fill,
-              ),
-              const SizedBox(
-                width: 4,
-              ),
-              const Text(
-                '..',
-                style: TextStyle(
-                  color: Colors.black45,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset(
+                  "assets/images/crown.png",
+                  height: 10,
+                  fit: BoxFit.fill,
                 ),
-              )
-            ],
+                const SizedBox(
+                  width: 4,
+                ),
+                const Text(
+                  '..',
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        Expanded(
-          child: Shimmer.fromColors(
-            baseColor: Colors.grey[300]!,
-            highlightColor: Colors.grey[100]!,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Container(
-                color: AppColors.bottom,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          '..',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+          Expanded(
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Container(
+                  color: AppColors.bottom,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            '..',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 8),
-                        Image.asset(
-                          'assets/images/progress_bar.png',
-                          height: 80,
-                          fit: BoxFit.fill,
-                        ),
-                        const SizedBox(height: 8),
-                        Image.asset(
-                          "assets/images/progress_bar.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ],
+                          const SizedBox(height: 8),
+                          Image.asset(
+                            'assets/images/progress_bar.png',
+                            height: 80,
+                            fit: BoxFit.fill,
+                          ),
+                          const SizedBox(height: 8),
+                          Image.asset(
+                            "assets/images/progress_bar.png",
+                            fit: BoxFit.fill,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        )
-      ]),
+          )
+        ],
+      ),
     );
   }
 }
