14b2cfb357feaa744843393169bab06e3503dcb1
Novian Andika <novian.andika@students.amikom.ac.id>
2023-04-17 13:16:23 +0700

    🎨 style(speaking_exercise_ui.dart): add scrollable view and remove unnecessary UI elements This commit adds a scrollable view to the SpeakingExercise UI and removes some unnecessary UI elements such as the "Speak" text and the microphone button. It also adds a new boolean variable `isSubmitButtonPressed` to track whether the submit button has been pressed or not. Finally, a new `ScrollController` is added to the UI to enable scrolling.
    🎨 style(speaking_exercise_ui.dart): improve UI layout and add conditional rendering for submit and next buttons
    The UI layout has been improved by adding a new text widget for the question and styling it with a larger font size and a different color. The UI now also supports conditional rendering of the submit and next buttons based on the state of the application. The submit button is now only visible when the user has finished speaking, and the next button is only visible after the user has submitted their answer.

--- lib/ui/pages/speaking_exercise/speaking_exercise_ui.dart
+++ lib/ui/pages/speaking_exercise/speaking_exercise_ui.dart
@@ -13,6 +13,9 @@ class SpeakingExercise extends StatefulWidget {
 
 class _SpeakingExerciseState extends State<SpeakingExercise> {
   bool isMicrophonePressed = false;
+  bool isSubmitButtonPressed = false;
+
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,117 +24,51 @@ class _SpeakingExerciseState extends State<SpeakingExercise> {
         title: 'Exercise',
         numberOfExercises: '1',
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 90),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 60,
-              ),
-              Container(
-                height: 250,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      blurRadius: 5,
-                      offset: const Offset(0, 2),
-                      spreadRadius: 0,
-                    ),
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      blurRadius: 10,
-                      offset: const Offset(0, 8),
-                      spreadRadius: -3,
-                    ),
-                  ],
-                ),
-                child: Center(
-                  child: Image.asset(
-                    "assets/images/chinese-people.png",
-                    height: 200,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              Row(
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 90),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset(
-                    "assets/images/volume_reading.png",
-                    height: 40,
-                    fit: BoxFit.contain,
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  Container(
+                    height: 250,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 5,
+                          offset: const Offset(0, 2),
+                          spreadRadius: 0,
+                        ),
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 10,
+                          offset: const Offset(0, 8),
+                          spreadRadius: -3,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        "assets/images/chinese-people.png",
+                        height: 200,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
                   ),
                   const SizedBox(
-                    width: 10,
+                    height: 60,
                   ),
-                  const Text(
-                    '你 叫 什么 名字？',
-                    style: TextStyle(
-                      fontSize: 36,
-                      color: AppColors.orangeColor,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              const Text(
-                'Answer :',
-                style: TextStyle(
-                  fontSize: 36,
-                  color: AppColors.blackColor,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              Visibility(
-                visible: isMicrophonePressed ? false : true,
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      MicrophoneImage(
-                        isPressed: isMicrophonePressed,
-                        onPressed: () {
-                          setState(
-                            () {
-                              isMicrophonePressed = true;
-                            },
-                          );
-                        },
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Text(
-                        'Speak',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: AppColors.yellowColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: isMicrophonePressed,
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  Row(
                     children: [
                       Image.asset(
                         "assets/images/volume_reading.png",
@@ -141,26 +78,197 @@ class _SpeakingExerciseState extends State<SpeakingExercise> {
                       const SizedBox(
                         width: 10,
                       ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('Play'),
+                      const Text(
+                        '你 叫 什么 名字？',
+                        style: TextStyle(
+                          fontSize: 36,
+                          color: AppColors.orangeColor,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ],
                   ),
-                ),
-              )
-            ],
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  const Text(
+                    'Answer :',
+                    style: TextStyle(
+                      fontSize: 36,
+                      color: AppColors.blackColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  Visibility(
+                    visible: isMicrophonePressed ? false : true,
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          MicrophoneImage(
+                            isPressed: isMicrophonePressed,
+                            onPressed: () {
+                              setState(
+                                () {
+                                  isMicrophonePressed = true;
+                                },
+                              );
+                            },
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text(
+                            'Speak',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.yellowColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Visibility(
+                    visible: isMicrophonePressed,
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/images/volume_reading.png",
+                            height: 40,
+                            fit: BoxFit.contain,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Play'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Visibility(
+                    visible: isSubmitButtonPressed,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          height: 80,
+                          width: 300,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: AppColors.lightGreen,
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  "Correct \nAnswer",
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 25,
+                                ),
+                                Image.asset(
+                                  "assets/images/volume_ok.png",
+                                  fit: BoxFit.fill,
+                                  height: 40,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              MicrophoneImage(
+                                isPressed: isMicrophonePressed,
+                                onPressed: () {
+                                  // setState(
+                                  //   () {
+                                  //     isMicrophonePressed = true;
+                                  //   },
+                                  // );
+                                },
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text(
+                                'Speak',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.yellowColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
       bottomNavigationBar: isMicrophonePressed
-          ? BottomNavigationBarButton(
-              color: AppColors.orangeColor,
-              name: 'Submit',
-              onTap: () {
-                // Navigator.pushNamed(context, '/speaking_exercise_result');
-              },
-            )
+          ? isSubmitButtonPressed
+              ? BottomNavigationBarButton(
+                  color: AppColors.greenColor,
+                  name: 'Next',
+                  onTap: () {
+                    setState(
+                      () {
+                        isSubmitButtonPressed = true;
+                      },
+                    );
+                  },
+                )
+              : BottomNavigationBarButton(
+                  color: AppColors.orangeColor,
+                  name: 'Submit',
+                  onTap: () {
+                    setState(
+                      () {
+                        isSubmitButtonPressed = true;
+                      },
+                    );
+
+                    _scrollController.animateTo(
+                      _scrollController.position.maxScrollExtent,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeOut,
+                    );
+
+                    // scroll through very bottom of the page
+                  },
+                )
           : const SizedBox(
               height: 0,
             ),
